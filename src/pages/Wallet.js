3fda791f6bb758cd30c6e4551382bd8392014599
Daniel Erhardt Cardoso <daniel.erhcardoso@gmail.com>
2022-05-21 14:38:10 -0300

    R7

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -8,6 +8,20 @@ class Wallet extends React.Component {
       <div>
         <Header />
         <ExpenseForm />
+
+        <table>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </table>
       </div>
     );
   }
